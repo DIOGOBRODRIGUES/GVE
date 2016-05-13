995f438a9d8334715504e1115b154522358553de
DIOGOBRODRIGUES <diogo.borbar@gmail.com>
2016-05-13 14:02:17 -0300

    Tela textual e Principal

--- GVE/src/br/ufrpe/sistema_vendas_estoque/gui/TelaTextual.java
+++ GVE/src/br/ufrpe/sistema_vendas_estoque/gui/TelaTextual.java
@@ -3,7 +3,20 @@ package br.ufrpe.sistema_vendas_estoque.gui;
 public class TelaTextual {
 	//comentario
 	public void executar(){
-		
+		System.out.println("*********************GVE****************");
+		System.out.println(" Sistema de controle de estoque");
+		System.out.println("*****************************************");
+		System.out.println("1-Clientes");
+		System.out.println("2-Vendas");
+	}
+	
+	public void menuCliente(){
+		System.out.println();
+		System.out.println("*********************GVE****************");
+		System.out.println("1-cadastrar");
+		System.out.println("2-remover");
+		System.out.println("3-Procurar");
+		System.out.println("4-Voltar");
 	}
 
 }
--- GVE/src/br/ufrpe/sistema_vendas_estoques/Principal.java
+++ GVE/src/br/ufrpe/sistema_vendas_estoques/Principal.java
@@ -1,13 +1,29 @@
 package br.ufrpe.sistema_vendas_estoques;
 
+import java.util.Scanner;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
+
 import br.ufrpe.sistema_vendas_estoque.gui.TelaTextual;
 
 public class Principal {
 	
 	//principal
 	public static void main(String[] args){
+		int menuOpcao;
 		TelaTextual tt = new TelaTextual();
 		tt.executar();
+		Scanner ler = new Scanner(System.in);
+		menuOpcao=ler.nextInt();
+		switch (menuOpcao) {
+		case 1:
+			tt.menuCliente();			
+			break;
+		default:
+			break;
+		}
 	}
+	
+	
 }
 
