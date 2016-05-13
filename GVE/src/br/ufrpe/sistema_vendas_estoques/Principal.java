5c8742655a7ee4439b1879c0a820ee39a900dd0f
jero <jero@172.16.206.70>
2016-05-13 11:27:18 -0300

    testando

--- GVE/src/br/ufrpe/sistema_vendas_estoques/Principal.java
+++ GVE/src/br/ufrpe/sistema_vendas_estoques/Principal.java
@@ -3,7 +3,8 @@ package br.ufrpe.sistema_vendas_estoques;
 import br.ufrpe.sistema_vendas_estoque.gui.TelaTextual;
 
 public class Principal {
-
+	
+	//principal
 	public static void main(String[] args){
 		TelaTextual tt = new TelaTextual();
 		tt.executar();
